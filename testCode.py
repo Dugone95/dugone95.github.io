57ee1f3120564f662cbcd5dea9b90fa88b805fac
dugone95 <corbinjtp@gmail.com>
2026-05-04 21:22:31 +1000

    testCode comit

--- testCode.py
+++ testCode.py
@@ -1,4 +1,10 @@
+
 import numpy as np
+from matplotlib import pyplot 
 
 array = np.arange(0, 10, 1)
-newArray = array*len(array)
+newArray = array * len(array)
+
+pyplot.scatter(array, newArray, marker = ".")
+pyplot.show() 
+
